c03fed471cddb802f1ec75e3d46572662473da9b
ashurohom <ashitosh.rohom@gmail.com>
2025-11-24 15:46:37 +0530

    update settings.py for railway deployment

--- blogapi/urls.py
+++ blogapi/urls.py
@@ -1,35 +1,18 @@
-"""
-URL configuration for blogapi project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
-from django.core.management import call_command
-from django.http import HttpResponse
 from django.http import JsonResponse
+from django.core.management import call_command
 
 def home(request):
     return JsonResponse({"message": "Backend Running Successfully!"})
 
 def run_migrations(request):
     call_command("migrate")
-    return HttpResponse("Migrations completed!")
+    return JsonResponse({"message": "Migrations completed!"})
 
 urlpatterns = [
-    path("", home),
-    path('admin/', admin.site.urls),
-    path('api/posts/', include('posts.urls')),
-    path("run-migrations/", run_migrations),
+    path('', home),                          # Homepage JSON message
+    path('admin/', admin.site.urls),         # Admin panel
+    path('api/posts/', include('posts.urls')),  # Posts API
+    path('run-migrations/', run_migrations), # Manual migrations URL
 ]
