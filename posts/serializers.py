649934f1f3b7dd65f166236f6afb1aefc40dd73a
ashurohom <ashitosh.rohom@gmail.com>
2025-11-26 16:28:48 +0530

    update setting.py

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,7 +1,12 @@
+# posts/serializers.py
 from rest_framework import serializers
 from .models import Post
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = "__all__"
+        fields = '__all__'
+        extra_kwargs = {
+            'description': {'required': False, 'allow_blank': True},
+            'due_date': {'required': False, 'allow_null': True},
+        }
